cb47c8c24f80017caa5b239c9b9e600ed7287ea1
ian512549 <ian512549@gmail.com>
2023-08-29 14:21:33 +0100

    sum_digits completed

--- __tests__/sum-digits.test.js
+++ __tests__/sum-digits.test.js
@@ -9,11 +9,25 @@ const sumDigits = require('../katas/sum-digits');
 
   E.g. sumDigits(1) should output 1
   E.g. sumDigits(9) should output 9
+    
 */
+describe("sumDigits()",() => {
+  test("returns the input number when passed a single digit number", () => {
+    const actual = sumDigits(1)
+    const expected = 1
+    expect(actual).toBe(expected)
+  })
+  test("returns the input number when passed a single digit number", () => {
+    const actual = sumDigits(9)
+    const expected = 9
+    expect(actual).toBe(expected)
+  })
+  })
 
 /*
   Once you have got the first test passing, then you can write your next one.
-  If you've already handled a single digit, your next test could test for multi-digit inputs.
+  If you've already handled a single digit, your next test could test for 
+  multi-digit inputs.
 
   E.g. sumDigits(99) should output 18
   E.g. sumDigits(123) should output 6
@@ -21,10 +35,22 @@ const sumDigits = require('../katas/sum-digits');
   HINT: Remember to see the test *fail* first, then write the code to pass the test
 
   Why this test?  
-  A multi-digit input means you now have to implement to 'addition' part of this function,
-  but you don't have to think about the logic for dealing with/ignoring non-digit characters yet
-  (that's for our next test!)
+  A multi-digit input means you now have to implement to 'addition' part of this 
+  function, but you don't have to think about the logic for dealing with/ignoring 
+  non-digit characters yet (that's for our next test!)
 */
+describe("sumDigits()", () => {
+  test("Adds multiple digits and returns the sum", () => {
+    const actual = sumDigits(99)
+    const expected = 18
+    expect(actual).toBe(expected)
+  })
+  test("Adds multiple digits and returns the sum", () => {
+    const actual = sumDigits(123)
+    const expected = 6
+    expect(actual).toBe(expected)
+  })
+})
 
 /*
   Once you have successfully passed the above test, then you can write your next test.
@@ -32,3 +58,17 @@ const sumDigits = require('../katas/sum-digits');
   
   E.g. sumDigits(10.5) should output 6
 */
+
+describe("sumDigits()", () => {
+  test("Adds multiple digits and returns the sum", () => {
+    const actual = sumDigits(10.5)
+    const expected = 6
+    expect(actual).toBe(expected)
+  })
+  test("Adds multiple digits and returns the sum", () => {
+    const actual = sumDigits("alphabet")
+    const expected = 'no digits'
+    expect(actual).toBe(expected)
+  })
+})
+
--- katas/sum-digits.js
+++ katas/sum-digits.js
@@ -1,3 +1,15 @@
-function sumDigits() {}
-
+function sumDigits(num) {
+    const string = num.toString()
+    //Convert the number into a string.
+    //Turn the string into an array of digits
+    const regex = /\d/
+    if (regex.test(string) === false) {
+        return 'no digits'
+    }
+    const array = string.split('')
+    const digitsArray = array.filter((element) => element.match(/\d/))
+    newArray = digitsArray.map((element) => Number(element))
+    const answer = newArray.reduce((accumulator, currentValue) => accumulator + currentValue)
+    return answer
+}
 module.exports = sumDigits;
