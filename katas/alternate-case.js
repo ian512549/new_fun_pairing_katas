c73e056af2512c7c50d3b54101fc99eea442964f
santhyt <san7859@hotmail.com>
2023-08-29 17:11:39 +0100

    progress on function coding in alternate-case.js file

--- katas/alternate-case.js
+++ katas/alternate-case.js
@@ -1,3 +1,28 @@
-function alternateCase() {}
+function alternateCase(string) {
+    const splitString = string.split('')
+    let altString = ''
+    let regex = /[a-z]/i
+    let filteredArray = splitString.filter((letter)=>regex.test(letter))
+
+    
+    for(const i in filteredArray) {
+        if(i % 2 === 0) {
+            filteredArray[i] = filteredArray[i].toUpperCase()            
+        } else {
+            filteredArray[i] = filteredArray[i].toLowerCase()
+        }
+        
+    }
+
+    console.log(filteredArray)
+    console.log(splitString)
+
+    for(const i in splitString) {
+        altString +=splitString[i]
+    }
+    
+    console.log(altString)
+    return altString
+}
 
 module.exports = alternateCase;
