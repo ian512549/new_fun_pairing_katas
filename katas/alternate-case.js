33fe0ed5e87b5b45887e1e30b94ff14ade56c9b3
santhyt <san7859@hotmail.com>
2023-08-30 10:33:05 +0100

    completed alternate-case.js

--- katas/alternate-case.js
+++ katas/alternate-case.js
@@ -2,19 +2,26 @@ function alternateCase(string) {
     const splitString = string.split('')
     let altString = ''
     let regex = /[a-z]/i
-    let filteredArray = splitString.filter((letter)=>regex.test(letter))
-
+    //let filteredArray = splitString.filter((letter)=>regex.test(letter))
+    let letterUpper = true
     
-    for(const i in filteredArray) {
-        if(i % 2 === 0) {
-            filteredArray[i] = filteredArray[i].toUpperCase()            
-        } else {
-            filteredArray[i] = filteredArray[i].toLowerCase()
+    for(const i in splitString) {
+                
+        if(regex.test(splitString[i])){
+            if(letterUpper === true) {
+                letterUpper = false
+                splitString[i] = splitString[i].toUpperCase()            
+            } else {
+                letterUpper = true
+                splitString[i] = splitString[i].toLowerCase()
+            }
+
         }
+
         
     }
 
-    console.log(filteredArray)
+   
     console.log(splitString)
 
     for(const i in splitString) {
