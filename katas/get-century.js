6fa2cf4544f1ba2af0b5e1856f4516d924a37c5e
santhyt <san7859@hotmail.com>
2023-08-30 11:24:25 +0100

    completed get-century.js

--- katas/get-century.js
+++ katas/get-century.js
@@ -1,3 +1,34 @@
-function getCentury() {}
+function getCentury(year) {
+    
+    //let century = (Math.floor(year/100) + 1).toString()
+
+    let century = (Math.floor(year/100))
+    console.log(century,'floor')
+
+    century++
+    console.log(century,'+1')
+
+    century = century.toString()
+    console.log(century,'toString')
+
+
+    switch(century[century.length-1]){
+    case '1':
+        century += 'st'
+    break
+
+    case '2':
+        century += 'nd'
+    break
+
+    case '3':
+        century += 'rd'
+    break
+    default:
+        century += 'th'
+    }
+
+    return century
+}
 
 module.exports = getCentury;
